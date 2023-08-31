d7d186db646496927eb8b21116022bf64d1f9b46
nrennie <nrennie35@gmail.com>
2023-08-31 18:58:21 +0100

    fix docs

--- R/add_whitespaces.R
+++ R/add_whitespaces.R
@@ -4,20 +4,20 @@
 #' all or a subset of columns.
 #' @param data input dataframe
 #' @param cols set of columns to apply transformation to. If `NULL`
-#' will apply to all columns. Default `NULL`,
-#' @param messyness Percentage of values to change. Must be
+#' will apply to all columns. Default `NULL`.
+#' @param messiness Percentage of values to change. Must be
 #' between 0 and 1. Default 0.1.
 #' @return a dataframe the same size as the input data.
 #' @export
 
-add_whitespace <- function(data, cols = NULL, messyness = 0.1) {
-  if (messyness < 0 || messyness > 1) {
-    stop("'messyness' must be between 0 and 1")
+add_whitespace <- function(data, cols = NULL, messiness = 0.1) {
+  if (messiness < 0 || messiness > 1) {
+    stop("'messiness' must be between 0 and 1")
   }
   if (is.null(cols)) {
     output <- data |>
-      dplyr::mutate(dplyr::across(everything(), ~dplyr::case_when(
-        runif(nrow(data)) <= messyness ~ paste0(.x, " "),
+      dplyr::mutate(dplyr::across(dplyr::everything(), ~dplyr::case_when(
+        runif(nrow(data)) <= messiness ~ paste0(.x, " "),
         TRUE ~ .x
       )))
   } else {
@@ -26,8 +26,8 @@ add_whitespace <- function(data, cols = NULL, messyness = 0.1) {
       stop("All elements of 'cols' must be a column name in 'data'")
     } else {
       output <- data |>
-        dplyr::mutate(dplyr::across(all_of(cols), ~dplyr::case_when(
-          runif(nrow(data)) <= messyness ~ paste0(.x, " "),
+        dplyr::mutate(dplyr::across(dplyr::all_of(cols), ~dplyr::case_when(
+          runif(nrow(data)) <= messiness ~ paste0(.x, " "),
           TRUE ~ .x
         )))
     }
